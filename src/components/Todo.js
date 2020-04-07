1052c987a107ae32d67f01871cdce106bcd71a3e
NaJ <naj10000@live.com>
2020-04-06 20:37:05 -0400

    realmvp

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+export default function Todo(props) {
+    
+    return (
+        <div 
+        
+        className={`todo${props.todo.completed ? ' completed': ''}`}
+        onClick={()=> props.toggleTodo(props.todo.id)}
+        >
+          <p>{props.todo.task}</p>  
+        </div>
+    )
+}
---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export class TodoForm extends Component {
+     constructor(){
+         super();
+         this.state = {
+             todo: ''
+         };
+     }
+    
+     handleChanges = e => {
+         this.setState({ [e.target.name]: e.target.value})
+     }
+
+     submitTodo = e => {
+         e.preventDefault();
+         this.setState({ todo: ''});
+         this.props.addTodo(e, this.state.todo)
+     }
+
+    render() {
+        return (
+            <form onSubmit={this.submitTodo}>
+                <input
+                    type="text"
+                    value={this.state.todo}
+                    name="todo"
+                    onChange={this.handleChanges}
+                
+                />
+                <button>Add</button>
+            </form>
+        )
+    }
+}
+
+export default TodoForm
